8d2fdfe05ed488f532c36498836f86dbba140ae6
KIRUBANANTHAN <144432314+KIRUBANANTHAN-D@users.noreply.github.com>
2026-01-07 14:01:58 +0530

    Backend

--- admin.py
+++ admin.py
@@ -1,23 +1,19 @@
 from django.contrib import admin
 from .models import Category, Cake, Branch ,Order
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
-
 
 @admin.register(Cake)
 class CakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price')
     list_filter = ('category',)
 
-
 @admin.register(Branch)
 class BranchAdmin(admin.ModelAdmin):
     list_display = ('name', 'location', 'is_main', 'open_time', 'close_time')
     list_editable = ('is_main',)
-
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -32,4 +28,5 @@ class OrderAdmin(admin.ModelAdmin):
         'total'
     )
     list_filter = ('delivery_type', 'egg_type', 'cake')
+
     search_fields = ('order_no', 'name', 'phone')
